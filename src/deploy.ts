e91d81146f99982c5061330389c82d74dc2fa9c7
twiistrzdev <twiistrz@gmail.com>
2022-07-01 14:58:53 +0800

    feat(deploy): add 'process error' logger

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,4 +1,9 @@
 import 'dotenv/config';
 import { Deploy } from './client/deploy.js';
+import logger from './utils/logger.js';
 
 await new Deploy().run();
+
+process.on(`rejectionHandled`, (error) => logger.error(error));
+process.on(`unhandledRejection`, (error) => logger.error(error));
+process.on(`uncaughtException`, (error) => logger.error(error));
